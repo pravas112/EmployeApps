b2c431fd7eba317d29ee1be97a4e4150ca765262
Prabhash <yprabhash170@gmail.com>
2026-05-10 22:13:26 +0530

    first1 commit

---
+++ src/main/java/com/servlet/EmployeeeDao.java
@@ -0,0 +1,23 @@
+package com.servlet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class EmployeeeDao {
+	private static final String url="jdbc:mysql://localhost:3306/employeedb";
+	private static final String username="root";
+	private static final String password="root1122";
+	
+	public static Connection createDBConnection() {
+		Connection connection=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			connection = DriverManager.getConnection(url, username, password);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+}
---
+++ src/main/java/com/servlet/EmployesServlete.java
@@ -0,0 +1,66 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/save")
+public class EmployesServlete extends HttpServlet{
+  
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		
+		String id = req.getParameter("id");
+		String name = req.getParameter("name");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		String city = req.getParameter("city");
+		String gender = req.getParameter("gender");
+		
+		
+		Connection dbConnection = EmployeeeDao.createDBConnection();
+		
+		
+		try {
+		PreparedStatement pst = dbConnection.prepareStatement("insert into employeees values(?,?,?,?,?,?)");
+		
+		pst.setString(1, id);
+		pst.setString(2, name);
+		pst.setString(3, password);
+		pst.setString(4, email);
+		pst.setString(5, gender);
+		pst.setString(6, city);
+		
+		
+		
+		int i =pst.executeUpdate();
+		
+		if(i>0) {
+			
+			resp.getWriter().print("<h2 style='color:green;font-weight:800'>Enter save sucess</h2>");
+			req.getRequestDispatcher("employee.html").include(req, resp);
+			
+		}else {
+			resp.getWriter().print("<h2 style='color:red;font-weight:800'>Enter save Notsucess</h2>");
+			req.getRequestDispatcher("employee.html").include(req, resp);
+			
+			
+		}
+		
+		
+		
+		} catch (Exception e) {
+			resp.getWriter().print("<h2 style='color:red;font-weight:800'>'"+e.getMessage()+"'</h2>");
+			req.getRequestDispatcher("employee.html").include(req, resp);
+			// TODO: handle exception
+		}
+	}
+	
+}
